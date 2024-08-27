148d73fd774798a12b1f55e6c908162c533f5e8a
sti <86592209+sti-233@users.noreply.github.com>
2024-08-27 14:45:01 +0800

    feat: Adjusting speed

--- Bilibili_ver_guess.sh
+++ Bilibili_ver_guess.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
-# Type the info
-version="8.11.0"  # version you want
-sn=15828351       # start sn
+# Initialize variables
+version="8.11.0"  # version you need
+sn_start=15828360  # starting sn
+batch_size=100      # number of sn per batch
 
 while true; do
-    url="https://dl.hdslb.com/mobile/pack/android64/$sn/iBiliPlayer-apinkRelease-$version-b$sn.apk"
-    wget -q $url -O iBiliPlayer-$sn.apk
-    if [ $? -eq 0 ]; then
-        echo "下载成功: $url"
-        break
-    else
+    # Generate a batch of sn
+    seq $sn_start $((sn_start + batch_size - 1)) | xargs -P $batch_size -I {} bash -c '
+        sn={}
+        url="https://dl.hdslb.com/mobile/pack/android64/$sn/iBiliPlayer-apinkRelease-'$version'-b$sn.apk"
+        
+        wget -q $url -O iBiliPlayer-$sn.apk
+        if [ $? -eq 0 ]; then
+            echo "下载成功: $url"
+            exit 0  # break
+        else
         echo "服务器上没有sn为$sn的文件"
-        rm -f iBiliPlayer-$sn.apk
+            rm -f iBiliPlayer-$sn.apk
+        fi
+    '
+    
+    # check if success
+    if ls iBiliPlayer-*.apk 1> /dev/null 2>&1; then
+        echo "下载成功，任务完成。"
+        break
     fi
-    sn=$((sn + 1))
+    
+    # Increase the range of sn
+    sn_start=$((sn_start + batch_size))
 done
